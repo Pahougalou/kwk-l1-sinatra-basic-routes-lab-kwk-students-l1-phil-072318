63fc0b7060a19abaf138ce92f83a3028c1de8cb5
Reice Griffin <reicelovesllamas@gmail.com>
2018-07-31 17:47:16 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  #homepage- whatever my local host is 
+  get '/'do
+    "Hello World"
+end
+
+
+#localhost/name 
+get '/name'do
+"My name is Reice."
+end
 end
